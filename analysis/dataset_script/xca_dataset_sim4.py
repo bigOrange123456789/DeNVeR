99eda50de3e9238bd75fc3c653987afabb248ff7
bigOrange123456789 <2814255951@qq.com>
2026-01-13 15:20:55 +0800

    使用了22帧固定时序长度的数据

--- analysis/dataset_script/xca_dataset_sim4.py
+++ analysis/dataset_script/xca_dataset_sim4.py
@@ -26,6 +26,16 @@ my_list=[]
 folder_path = "xca_dataset"#input("请输入文件夹路径：")
 gapMin=999 #距离末尾的最近距离
 gapPre=999 #距离0号帧的最近距离
+frameIdMax=-1
+frameIdMin=999
+frameIdMax0=-1
+frameIdMin0=999
+frameIdMax1=-1
+frameIdMin1=999
+gapLenMin=999
+gapLenMax=-1
+videoMax=-1
+videoMin=999
 for item in os.listdir(folder_path):
     path_gt_user=folder_path+"/"+item+"/ground_truth"#/CVAI-1207LAO44_CRA29"
     videoId_list=os.listdir(path_gt_user)
@@ -45,6 +55,9 @@ for item in os.listdir(folder_path):
                 
             NUMBER=NUMBER+1
             source_path = os.path.join(folder_path+"/"+item+"/images",videoId)
+            l = len(os.listdir(source_path))#视频长度
+            if videoMax<l:videoMax=l
+            if videoMin>l:videoMin=l
             src_id_max=0
             # src_id_min=999
             for name_src_img in os.listdir(source_path):
@@ -65,12 +78,51 @@ for item in os.listdir(folder_path):
             source_path2=source_path.split("xca_dataset/")[1]
             # print(source_path2)
             my_list.append(source_path2)
+
+
+            src_gt_path = os.path.join(folder_path +"/"+item+"/ground_truth",videoId)
+            for i in os.listdir(src_gt_path):
+                i=i.split(".")[0]
+                i=int(i)
+                if i>frameIdMax:frameIdMax=i
+                if i<frameIdMin:frameIdMin=i
+            if True:
+                i0=os.listdir(src_gt_path)[0]
+                i0=i0.split(".")[0]
+                i0=int(i0)
+                if i0>frameIdMax0:frameIdMax0=i0
+                if i0<frameIdMin0:frameIdMin0=i0
+            if True:
+                i1=os.listdir(src_gt_path)[-1]
+                i1=i1.split(".")[0]
+                i1=int(i1)
+                if i1>frameIdMax1:frameIdMax1=i1
+                if i1<frameIdMin1:frameIdMin1=i1
+                # print(i)
+            # print(os.listdir(src_gt_path))
+            gapLen = i1-i0+1
+            if gapLen<gapLenMin:gapLenMin=gapLen
+            if gapLen>gapLenMax:gapLenMax=gapLen
+            framelen=22#总帧数
+            if (i1-i0+1)>framelen:
+                print(videoId,"err1")
+                exit(0)
+            if not (i1>=framelen-1 or l>=framelen):
+                print(videoId,"err2")
+                exit(0)
+print("framelen",framelen)
 print("视频数量",NUMBER)
 print("图片数量",NUMBER2)
 print("gapMin",gapMin)
 print("gapPre",gapPre)
-# exit(0)
+print("gapLenMin",gapLenMin)
+print("gapLenMax",gapLenMax)
 
+print("关键帧编号最小最大数值:",frameIdMin,frameIdMax)
+print("关键帧编号0最小最大数值:",frameIdMin0,frameIdMax0)
+print("关键帧编号1最小最大数值:",frameIdMin1,frameIdMax1)
+print("视频长度  最小最大数值:",videoMin,videoMax)
+# exit(0)
 ##############################################################################################################
 def copy_folder_contents(src_folder, dest_folder):
     # 确保源文件夹存在
@@ -116,7 +168,7 @@ NUMBER=0
 NUMBER2=0
 my_list=[]
 folder_path = "xca_dataset" # input("请输入文件夹路径：")
-folder2_path = "xca_dataset_sim2" 
+folder2_path = "xca_dataset_sim4" 
 for item in os.listdir(folder_path):
     path_gt_user=folder_path+"/"+item+"/ground_truth"#/CVAI-1207LAO44_CRA29"
     path_gt_user2=folder2_path+"/"+item+"/ground_truth"#/CVAI-1207LAO44_CRA29"
@@ -137,13 +189,21 @@ for item in os.listdir(folder_path):
                 if gt_id<gt_id_min:gt_id_min=gt_id
             # print("gt_id_max",gt_id_max)
             # print("gt_id_min",gt_id_min)
+
+            startPos=0#起始帧位置
+            # framelen=40#总帧数
+            src_gt_path = os.path.join(folder_path +"/"+item+"/ground_truth",videoId)
+            i1 = int(os.listdir(src_gt_path)[-1].split(".")[0])#末尾关键帧的编号
+            if i1>=40: startPos=i1-40
+
                 
             NUMBER=NUMBER+1
             if len(videoId.split("CATH"))==1:
                 source_path = os.path.join(folder_path+"/"+item+"/images",videoId)
                 dest_path = os.path.join(folder2_path+"/"+item+"/images",videoId)
-                startPos = gt_id_min-8
-                for i in range(gt_id_min-8,gt_id_max+1+gapMin):
+                # startPos = gt_id_min-8 # 起点是 第一关键帧的前8帧
+                # for i in range(gt_id_min-8,gt_id_max+1+gapMin):
+                for i in range(startPos, startPos+framelen):
                     imgfileName=str(i).zfill(5)+".png"
                     img_path_src=os.path.join(source_path,imgfileName)
                     # img_path_des=os.path.join(dest_path  ,imgfileName)
--- nir/param - 副本 (5).py
+++ nir/param - 副本 (5).py
@@ -34,15 +34,15 @@ if True:
     # 定义多个配置
     configs = [#在短视频数据上的测试结果
 
-        # { #刚体软体动态特征MASK
+        # { #测试一下纹理网络能否将层数改为1 #0.763
         #     "decouple":{ # 解耦
-        #         "tag":"A21-11",#企图复现20-01的指标，刚体层静止
+        #         "tag":"A21-09",#企图复现20-01的指标，刚体层静止
         #         "de-rigid":"1_sim",#去噪框架
         #         "epoch":2000,#6000,#4000,#2000,          #只兼容了startDecouple1 #recon_all=0.00011
         #         # 1 模型本身
         #         # 1.1 刚体模块
         #         "NUM_rigid":1,#只有一个运动的刚体
-        #         "configRigid":{ #单个刚体层的参数 #有整体运动、但是没有局部运动
+        #         "configRigid":{ #单个刚体层的参数
         #             # 纹理
         #             'hidden_layers_map':2,#1,#2,#4,#32,#4,
         #             'hidden_features_map': 8*512,#256,#64,#8,#2*4*512,#16*4*512,#128,#512, #128,
@@ -59,9 +59,103 @@ if True:
         #             "loss_recon_all_type":"MSE",#{"myLog" 学习能力不如MSE, "MSE", "atten_d"} #我猜测均方误差更关注背景、注意力损失更关注血管
         #         }, 
         #         "openLocalDeform":False, #True,
-        #         "stillness":    False,#True,#False, #True,#False,#不取消运动约束
-        #         "stillnessFristLayer":False,#True,#:False, #True,#False,#并无意义，要和stillness保持一致
-        #         "use_dynamicFeatureMask":True,
+        #         "stillness":    True,#False, #True,#False,#不取消运动约束
+        #         "stillnessFristLayer":True,#:False, #True,#False,#并无意义，要和stillness保持一致
+        #         # 1.2 软体模块
+        #         "NUM_soft":1,#1,
+        #         "configSofts":{ # 软体
+        #             "layer":{
+        #                 # 纹理
+        #                 'hidden_layers_map':2, # 1, # 2, # 4, # 32, # 4,
+        #                 'hidden_features_map': 8*512, # 将隐含层特征维度变为1/8
+        #                 # 整体运动
+        #                 'hidden_layers_global':2, 
+        #                 'hidden_features_global':8*128, 
+        #                 # 局部运动
+        #                 'hidden_layers_local':2,
+        #                 'hidden_features_local':8*128, # Mask遮挡
+        #             },
+        #             "useSoftMask" : False, #无法生成有意义的MASK
+        #             "layerMask":{ #无效
+        #                 "hidden_features": 64,#8,#256,#3*256,#7*256, 
+        #                 "hidden_layers": 2, 
+        #                 "use_residual": False, # 似乎还有负面作用
+        #                 "posEnc":{ # 有显著作用
+        #                     "num_freqs_pos":10, #3
+        #                     "num_freqs_time":10, #4, #1 #后面要通过这里测试时序编码能否提升效果
+        #                     "APE":False,
+        #                 }, 
+        #                 "gradualImageLayers":False,
+        #                 "use_maskP":False,
+        #             },
+        #         },
+        #         # 1.3 流体模块
+        #         "NUM_fluid":0, # 0.00019 -> 0.00016、0.00015
+        #         "configFluids":{ #参数数量
+        #             "hidden_features": 64,#8,#256,#3*256,#7*256, 
+        #             "use_featureMask":False,
+        #             "hidden_layers": 2, 
+        #             "use_residual": False, # 似乎还有负面作用
+        #             "posEnc":{ # 有显著作用
+        #                 "num_freqs_pos":10, #3
+        #                 "num_freqs_time":10, #4, #1 #后面要通过这里测试时序编码能否提升效果
+        #                 "APE":False,
+        #             }, 
+        #             "gradualImageLayers":False,
+        #             "use_maskP":False,
+        #         }, # 现在的首要问题是无损失地拟合出来视频
+        #         # 2.损失函数
+        #         "useSmooth":False, #不进行平滑约束
+        #         "weight_smooth":0.1**7,#0.001,#0.1, #1,始终固定 #10,始终固定 #0.1,
+        #         "interval":0.1,#将计算平滑损失的步长由1改为0.5
+        #         "lossType":2,
+        #         "lossParam":{
+        #             "rm":None,#"F",#"R,S",
+        #             "ra":"R,S",
+        #             # "blank":"F",
+        #             }, 
+        #         "maskPath_pathIn":None,#"A20-10-best1.rigid.non1", # 当"rm"==None的时候,没有用处
+        #         "useMask":True, #只有lossType==1的时候才有效
+        #         ########################
+        #         "de-soft":None,
+        #     },
+        #     "name": "A21-09", #提高模型的拟合能力
+        #     "precomputed": False,
+        #     "noise_label":"A21-09.rigid",
+        #     "input_mode": "A21-09.rigid.non1",
+        #     "norm_method": norm_calculator.calculate_mean_variance,
+        #     "binarize": True,
+        #     "inferenceAll": True,#False,
+        #     "mergeMask": False,
+        # },
+
+        # { #流体渐进式特征MASK
+        #     "decouple":{ # 解耦
+        #         "tag":"A21-10",#企图复现20-01的指标，刚体层静止
+        #         "de-rigid":"1_sim",#去噪框架
+        #         "epoch":2000,#6000,#4000,#2000,          #只兼容了startDecouple1 #recon_all=0.00011
+        #         # 1 模型本身
+        #         # 1.1 刚体模块
+        #         "NUM_rigid":1,#只有一个运动的刚体
+        #         "configRigid":{ #单个刚体层的参数
+        #             # 纹理
+        #             'hidden_layers_map':2,#1,#2,#4,#32,#4,
+        #             'hidden_features_map': 8*512,#256,#64,#8,#2*4*512,#16*4*512,#128,#512, #128,
+        #             # 'hidden_features_map': 512, #将隐含层特征维度变为1/8
+        #             # 整体运动
+        #             'hidden_layers_global':2,
+        #             'hidden_features_global':128,
+        #             # 局部运动（刚体层哪来的局部运动）
+        #             'hidden_layers_local':2,
+        #             'hidden_features_local':128,
+        #         }, 
+        #         "configRigids":{ # 整个刚体层模块的参数
+        #             "useLocal":False, #不使用局部运动
+        #             "loss_recon_all_type":"MSE",#{"myLog" 学习能力不如MSE, "MSE", "atten_d"} #我猜测均方误差更关注背景、注意力损失更关注血管
+        #         }, 
+        #         "openLocalDeform":False, #True,
+        #         "stillness":    True,#False, #True,#False,#不取消运动约束
+        #         "stillnessFristLayer":True,#:False, #True,#False,#并无意义，要和stillness保持一致
         #         # 1.2 软体模块
         #         "NUM_soft":1,#1,
         #         "configSofts":{ # 软体
@@ -109,10 +203,9 @@ if True:
         #         # 2.损失函数
         #         "useSmooth":False, #不进行平滑约束
         #         "weight_smooth":0.1**7,#0.001,#0.1, #1,始终固定 #10,始终固定 #0.1,
-        #         "weight_concise":0.00001,
         #         "interval":0.1,#将计算平滑损失的步长由1改为0.5
         #         "lossType":2,
-        #         "lossParam":{#这里写错了
+        #         "lossParam":{
         #             "rm":"F",#"R,S",
         #             "ra":"R,S",
         #             # "blank":"F",
@@ -122,21 +215,20 @@ if True:
         #         ########################
         #         "de-soft":None,
         #     },
-        #     "name": "A21-11", #提高模型的拟合能力
+        #     "name": "A21-10", #提高模型的拟合能力
         #     "precomputed": False,
-        #     "noise_label":"A21-11.rigid",
-        #     "input_mode": "A21-11.rigid.non1",
+        #     "noise_label":"A21-10.rigid",
+        #     "input_mode": "A21-10.rigid.non1",
         #     "norm_method": norm_calculator.calculate_mean_variance,
         #     "binarize": True,
         #     "inferenceAll": True,#False,
         #     "mergeMask": False,
         # },
-        
-        ####################### xca_dataset_sim4(固定视频长度) ####################### 
-        ##########################  DeNVeR.22   ##########################  
+
+
         { #刚体软体动态特征MASK
             "decouple":{ # 解耦
-                "tag":"A22-01",#企图复现20-01的指标，刚体层静止
+                "tag":"A21-11",#企图复现20-01的指标，刚体层静止
                 "de-rigid":"1_sim",#去噪框架
                 "epoch":2000,#6000,#4000,#2000,          #只兼容了startDecouple1 #recon_all=0.00011
                 # 1 模型本身
@@ -222,10 +314,10 @@ if True:
                 ########################
                 "de-soft":None,
             },
-            "name": "A22-01", #提高模型的拟合能力
+            "name": "A21-11", #提高模型的拟合能力
             "precomputed": False,
-            "noise_label":"A22-01.rigid",
-            "input_mode": "A22-01.rigid.non1",
+            "noise_label":"A21-11.rigid",
+            "input_mode": "A21-11.rigid.non1",
             "norm_method": norm_calculator.calculate_mean_variance,
             "binarize": True,
             "inferenceAll": True,#False,
